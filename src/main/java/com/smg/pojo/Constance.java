be9cc070bbcb25366a8dc9acec8b189412cd7af4
chenxy@shaiic.com <2500941316@qq.com>
2020-09-07 10:59:50 +0800

    1.1

--- src/main/java/com/smg/pojo/Constance.java
+++ src/main/java/com/smg/pojo/Constance.java
@@ -4,7 +4,7 @@ public class Constance {
     private Constance(){
         throw new IllegalStateException("Constance class");
     }
-    public static final String IP="10.27.137.221";
-    public static final String PORT="8080";
+    public static final String IP="10.2.1.13";
+    public static final String PORT="1028";
     public static final String PCMPATH="/opt/";
 }
