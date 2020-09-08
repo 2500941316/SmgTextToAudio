6d76728903444a49b695f31435e7918e8feadb01
chenxy@shaiic.com <2500941316@qq.com>
2020-09-08 13:55:22 +0800

    1.2

--- src/main/java/com/smg/pojo/Constance.java
+++ src/main/java/com/smg/pojo/Constance.java
@@ -5,6 +5,6 @@ public class Constance {
         throw new IllegalStateException("Constance class");
     }
     public static final String IP="10.27.143.33:1028";
-    public static final String PCMPATH="/opt/";
+    public static final String PCMPATH="/opt/src/products/";
     public static final String MP3OUTPUTPATH="/opt/src/products/";
 }
--- src/main/java/com/smg/service/TextToRedioServiceImpl.java
+++ src/main/java/com/smg/service/TextToRedioServiceImpl.java
@@ -140,7 +140,7 @@ public class TextToRedioServiceImpl implements TextToRedioInterface {
         String pcmFile = Constance.PCMPATH + textInfo.getPcmMD5FileName();
         if (pcmToMp3(pcmFile)) {
             logger.info("转码成功");
-            return "http://mp3File/"+textInfo.getPcmMD5FileName().substring(0, pcmFile.lastIndexOf('.')) + ".mp3";
+            return "http://mp3File/"+textInfo.getPcmMD5FileName() + ".mp3";
         } else {
             throw new BusinessException(Exceptions.SERVER_FFMPEG_ERROR.getEmsg());
         }
